4df23e27b525f85ab32657fe67f158997747b969
DANIEL.ORTEGA <oh.dany.mx@gmail.com>
2020-09-26 14:20:18 -0500

    Proyecto inicial de reacto con redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import imagen from './img/pokemon3.jpg';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import CantidadPokemon from './components/CantidadPokemon';
+import CompraPokemon from './components/CompraPokemon';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
+  console.log(store.getState());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="app container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card mt-5" style={{ maxWidth: '370px' }}>
+              <div className="row no-gutters">
+                <div className="col-4">
+                  <img src={imagen} alt="pokemon" className="card-img" />
+                </div>
+                <div className="col-8">
+                  <div className="card-body">
+                    <div className="card-title h3 text-center">
+                      <CantidadPokemon />
+                    </div>
+                    <CompraPokemon />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/components/CantidadPokemon.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const CantidadPokemon = () => {
+  return (
+    <Fragment>
+      <div>Unidades: 30</div>
+    </Fragment>
+  );
+};
+
+export default CantidadPokemon;
