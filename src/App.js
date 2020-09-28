0fa4aad35c759210538c6aa836c791b544c04eb7
DANIEL.ORTEGA <oh.dany.mx@gmail.com>
2020-09-27 21:57:23 -0500

    Conectar con la API de Pokemon

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import CompraPokemonHook from './components/CompraPokemon.hook';
 // Redux
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import BuscadorPokemon from './components/buscador/BuscadorPokemon';
+import ResltadoPokemon from './components/buscador/ResltadoPokemon';
 
 function App() {
   console.log(store.getState());
@@ -36,6 +38,12 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="col-12 mt-4 border-top pt-3">
+            <BuscadorPokemon />
+          </div>
+          <div className="col-12">
+            <ResltadoPokemon />
           </div>
         </div>
       </div>
---
+++ src/components/buscador/BuscadorPokemon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const BuscadorPokemon = () => {
+  return (
+    <div className="form-group">
+      <label htmlFor="buscar_pokemon" className="text-white">
+        Buscar Pokemon
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="buscar_pokemon"
+        value="Pikachu"
+      />
+      <button className="btn btn-primary mt-3">Enviar</button>
+    </div>
+  );
+};
+
+export default BuscadorPokemon;
