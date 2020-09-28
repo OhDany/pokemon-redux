84afa2b4957786a82ca2af496ac47a5b608ab9b8
DANIEL.ORTEGA <oh.dany.mx@gmail.com>
2020-09-27 22:56:52 -0500

    Generar los reducer de buscador

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import buscador from './bucadorReducer';
 import game_shop from './gameShopReducer';
 
 const rootReducer = combineReducers({
   game_shop,
+  buscador,
 });
 
 export default rootReducer;
